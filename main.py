6a781d1dd9d14c1015e4f02c9b06bf043a564cee
Michael Lucas <michiqt@live.de>
2020-01-22 19:54:16 +0100

    Welcome (ITS SOMETHING)

---
+++ main.py
@@ -0,0 +1,11 @@
+
+def showWelcome()   :
+    divier = "=============================="
+
+    print(divier)
+    print("        # WILLKOMMEN #")
+    print("       Ordner-Erstellung")
+    print("    (c) 2020 Michael Lucas")
+    print(divier)
+
+showWelcome()
