58ea23032d809bb8670610acd6b9e0a86fe9e759
Michael Lucas <michiqt@live.de>
2020-01-23 16:02:19 +0100

    Ordnerstruktur

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
 import folders
+import names
 
-def showWelcome()   :
+def showWelcome():
     divier = "=============================="
 
     print(divier)
@@ -10,6 +11,21 @@ def showWelcome()   :
     print("    (c) 2020 Michael Lucas")
     print(divier)
 
-    folders.createFolder("../", "yehaw")
+def ask(what):
+    return str(input(what));
 
-showWelcome()
+def start():
+    showWelcome()
+
+    year = ask("Fuer welches Jahr sollen die Ordner erstellt werden? ")
+    quarter = ask("Fuer welches Quartal sollen die Ordner erstellt werden? ")
+    quarterFolder = year + "/" + names.quarterNames[int(quarter) - 1]
+
+    folders.createFolder("./", year)
+    folders.createFolder("./", quarterFolder)
+
+    for fn in names.folderNames:
+        folders.createFolder("./", quarterFolder + "/" + fn)
+
+
+start()
---
+++ names.py
@@ -0,0 +1,2 @@
+quarterNames = ["1 Quartal", "2 Quartal", "3 Quartal"]
+folderNames = ["Eingehend", "Ausgehend", "Konto"]
