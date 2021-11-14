decc4ed9fb8c7a2ed07b25f939bb085cb7ebf32c
Michael Lucas <michiqt@live.de>
2021-11-14 12:46:01 +0100

    Struktur, Kommentare

--- helper/folders.py
+++ helper/folders.py
@@ -1,6 +1,7 @@
 import os
+import helper.names
 
-# Ordnerstruktur
+# structure
 """
 2020
 ├── 1 Quartal
@@ -16,14 +17,41 @@ import os
 │   └── Konto
 """
 
+# Checks if a folder exists
 def folderExists(path):
     return os.path.exists(path) and os.path.isdir(path)
 
+# Creates a folder
 def createFolder(path, name):
     folderName = path + "/" + name
     if not folderExists(folderName):
         os.makedirs(folderName)
 
+# Deletes a folder
 def deleteFolder(path):
     if folderExists(path):
         os.rmdir(path)
+
+        
+# Runs through all quarters and creates folders 
+# (structure: See folders.py)
+def createBhFolders(year):
+    for i in range(1,5):
+        quarter = helper.names.findQuarter(str(i) + " Quartal")
+        quarterFolder = year + "/" + quarter[0] + "/"
+        months = quarter[1]
+
+        # Create year, quarter and account
+        createFolder("./", year)
+        createFolder("./", quarterFolder)
+        createFolder(quarterFolder,"Konto")
+
+        # Create months
+        for month in months:
+            createFolder(quarterFolder, month)            
+
+            # Create inbox and outbox folder
+            for folderName in helper.names.folderNames:
+                createFolder(quarterFolder + month + "/", folderName)
+
+    print("Fertig!")
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-from helper import names, folders, functions
+from helper import folders, functions
 
 def showWelcome():
     divier = "=============================="
@@ -7,37 +6,16 @@ def showWelcome():
     print(divier)
     print("        # WILLKOMMEN #")
     print("       Ordner-Erstellung")
-    print("    (c) 2020 Michael Lucas")
+    print("    (c) 2020-2021 Michael Lucas")
     print(divier)
 
-
-# Läuft alle Quartale durch und erstellt Ordner (Struktur: Sehe folders.py)
-def createFolders(year):
-    for i in range(1,5):
-        quarter = names.findQuarter(str(i) + " Quartal")
-        quarterFolder = year + "/" + quarter[0] + "/"
-        months = quarter[1]
-
-        # Jahr, Quartal und Konto erstellen
-        folders.createFolder("./", year)
-        folders.createFolder("./", quarterFolder)
-        folders.createFolder(quarterFolder,"Konto")
-
-        # Monate erstellen
-        for month in months:
-            folders.createFolder(quarterFolder, month)            
-
-            # Eingang- und Ausgang- Ordner erstellen
-            for folderName in names.folderNames:
-                folders.createFolder(quarterFolder + month + "/", folderName)
-
-def start():
+def main():
     showWelcome()
 
     year = "0"
     while not functions.validateYear(year):
         year = functions.ask("Fuer welches Jahr sollen die Ordner erstellt werden? ")
     
-    createFolders(year)
+    folders.createBhFolders(year)
 
-start()
+main()
